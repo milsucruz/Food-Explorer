93bbf424f85a28dd04f8ae25c315fcd979eeefb7
Edmilson Cruz <edmilsoncruz.dev@gmail.com>
2023-01-16 16:52:03 -0300

    Front-end base

---
+++ FE - Front/src/pages/Details/index.jsx
@@ -0,0 +1,14 @@
+//
+import { Container } from "./styles"
+import { Button } from "../../components/Button"
+
+export function Details () {
+  return(
+    <Container>
+      <h1>Hello word!</h1>
+      <span>test</span>
+
+      <Button title="Cadastrar" />
+    </Container>
+  )
+}
