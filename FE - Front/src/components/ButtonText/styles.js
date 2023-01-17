4722e9bddb9bbb4a8f4d9a19d63935113a8a6ecc
Edmilson Cruz <edmilsoncruz.dev@gmail.com>
2023-01-17 15:43:04 -0300

    Component ButtonText

---
+++ FE - Front/src/components/ButtonText/styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Container = styled.button`
+  background: none;
+  color: ${({theme}) => theme.COLORS.WHITE};
+  border: none;
+  font-size: 1.4rem;
+`;
--- FE - Front/src/pages/Details/index.jsx
+++ FE - Front/src/pages/Details/index.jsx
@@ -2,11 +2,14 @@
 import { Container } from "./styles"
 import { Header } from "../../components/Header"
 import { Button } from "../../components/Button"
+import { ButtonText } from "../../components/ButtonText"
 
 export function Details () {
   return(
     <Container>
       <Header/>
+      <Button title="Test Button" />
+      <ButtonText title="Test ButtonText" />
     </Container>
   )
 }
