e268724c00306ac0747d4fd4a8fc3c97dfdcc8a1
Edmilson Cruz <edmilsoncruz.dev@gmail.com>
2023-01-17 18:02:39 -0300

    SignIn Page

--- FE - Front/src/main.jsx
+++ FE - Front/src/main.jsx
@@ -5,13 +5,14 @@ import theme from './styles/theme'
 import  GlobalStyles from './styles/global' 
 
 import {Details} from './Pages/Details'
-import {Signin} from './Pages/Signin'
+import {SignIn} from './Pages/SignIn'
+import {SignUp} from './Pages/SignUp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <Signin />
+      <SignUp />
     </ThemeProvider>
   </React.StrictMode>
 )
--- FE - Front/src/pages/SignUp/index.jsx
+++ FE - Front/src/pages/SignUp/index.jsx
@@ -4,7 +4,7 @@ import { Button } from '../../components/Button'
 import { ButtonText } from '../../components/ButtonText'
 import { Container, Logo, Form } from "./styles";
 
-export function SignIn(){
+export function SignUp(){
   return(
     <Container>
       <Logo>
@@ -15,7 +15,12 @@ export function SignIn(){
       </Logo>
 
       <Form>
-        <h1>Faça login</h1>
+        <h1>Crie sua conta</h1>
+
+        <div className="inputs">
+          <p>Seu nome</p>
+          <Input placeholder="Exemplo: Maria da Silva" />
+        </div>
 
         <div className="inputs">
           <p>Email</p>
@@ -27,9 +32,9 @@ export function SignIn(){
           <Input placeholder="No mínimo 6 caracteres" />
         </div>
 
-        <Button title="Entrar" />
+        <Button title="Crie a sua conta" />
 
-        <ButtonText title="Criar uma conta"/>
+        <ButtonText title="Já tenho uma conta"/>
 
       </Form>
     </Container>
