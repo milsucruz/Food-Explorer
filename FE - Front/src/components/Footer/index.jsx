e2a9c59b3ef685d3860ae24cc45806846d0ca090
Edmilson Cruz <edmilsoncruz.dev@gmail.com>
2023-02-02 19:04:02 -0300

    Footer media queries

--- FE - Front/src/components/Footer/index.jsx
+++ FE - Front/src/components/Footer/index.jsx
@@ -1,16 +1,18 @@
-import { Container, Logo } from "./styles";
+import { Container, Content, Logo } from "./styles";
 import logo from '../../assets/logoGray.svg'
 
 export function Footer() {
   return(
     <Container>
 
-      <Logo>
-        <img src={logo} alt="Logo do Food Explorer" />
-        <h1>Food Explorer</h1>
-      </Logo>
+      <Content>
+        <Logo>
+          <img src={logo} alt="Logo do Food Explorer" />
+          <h1>Food Explorer</h1>
+        </Logo>
 
-      <p>© 2023 - Todos os direitos reservados.</p>
+        <p>© 2023 - Todos os direitos reservados.</p>
+      </Content>
 
     </Container>
   )
--- FE - Front/src/components/Footer/styles.js
+++ FE - Front/src/components/Footer/styles.js
@@ -1,15 +1,29 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  width: 100%;
+  height: 7.7rem;
+
+  background-color: ${({theme}) => theme.COLORS.BACKGROUND_800};
+`;
+
+export const Content = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-
-  width: 100%;
-  height: 7.7rem;
   padding: 2.34rem 12.3rem;
 
-  background-color: ${({theme}) => theme.COLORS.BACKGROUND_800};
+  @media (max-width: 1000px) {
+    padding: 2.34rem 5rem;
+
+    > p {
+      font-size: 1rem;
+    }
+  }
+
+  @media (max-width: 500px) {
+    padding: 2.34rem 2rem;
+  }
 `;
 
 export const Logo = styled.div`
@@ -18,6 +32,18 @@ export const Logo = styled.div`
   gap: 1.1rem;
 
   > h1 {
+    font-size: 2.4rem;
     color: ${({theme}) => theme.COLORS.GRAY_900}
+  }
+
+  @media (max-width: 1000px) {
+    > h1 {
+      font-size: 1.5rem;
+    }
+
+    > img {
+      height: 1.8rem;
+      width: 1.8rem;
+    }
   }
 `;
